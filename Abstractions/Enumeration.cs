12412b974db6c9f980ac9174192d1e1c0de5947f
lulus <snys98@outlook.com>
2020-12-04 10:42:31 +0800

    init

---
+++ Abstractions/Enumeration.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+
+namespace Geexbox.Common
+{
+    /// <summary>
+    /// simulation type for typescript string enum
+    /// </summary>
+    [JsonConverter(typeof(EnumerationConverter))]
+    [ModelBinder(BinderType = typeof(EnumerationBinder))]
+    public abstract class Enumeration : ValueObject
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        protected Enumeration(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public static implicit operator string(Enumeration value)
+        {
+            return value?.Value;
+        }
+
+        protected override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Name;
+            yield return Value;
+        }
+
+        public static bool TryParse<TImplemetions>(string value, out TImplemetions result) where TImplemetions : Enumeration
+        {
+            result = Enumeration.GetAll<TImplemetions>().FirstOrDefault(x => x.Value.CompareTo(value) == 0);
+            return result != default(TImplemetions);
+        }
+
+        public static TImplemetions Parse<TImplemetions>(string value) where TImplemetions : Enumeration
+        {
+            var result = Enumeration.GetAll<TImplemetions>().First(x => x.Value.CompareTo(value) == 0);
+            return result;
+        }
+
+        public static IEnumerable<TImplemention> GetAll<TImplemention>() where TImplemention : Enumeration
+        {
+            var type = typeof(TImplemention);
+            var typeInfo = type.GetTypeInfo();
+            var fields = typeInfo.GetFields(BindingFlags.Public |
+                                                      BindingFlags.Static |
+                                                      BindingFlags.DeclaredOnly);
+            foreach (var info in fields)
+            {
+                if (info.GetValue(default(TImplemention)) is TImplemention locatedValue)
+                {
+                    yield return locatedValue;
+                }
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            var otherValue = obj as Enumeration;
+            if (otherValue == null)
+            {
+                return false;
+            }
+            var typeMatches = GetType() == obj.GetType();
+            var valueMatches = Value.Equals(otherValue.Value);
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<string>.Default.GetHashCode(Value);
+        }
+
+        public int CompareTo(object other)
+        {
+            return Value.CompareTo(((Enumeration)other).Value);
+        }
+
+        // Other utility methods ...
+    }
+
+    public class EnumerationBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var modelName = bindingContext.ModelName;
+
+            // Try to fetch the value of the argument by name
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+            var value = valueProviderResult.FirstValue;
+
+            // Check if the argument value is null or empty
+            if (string.IsNullOrEmpty(value))
+            {
+                return Task.CompletedTask;
+            }
+
+            var locatedValue = typeof(Enumeration).GetMethod(nameof(Enumeration.Parse))
+                ?.MakeGenericMethod(bindingContext.ModelType).Invoke(null, new[] { value });
+
+            if (locatedValue == default)
+                return Task.CompletedTask;
+            bindingContext.Result = ModelBindingResult.Success(locatedValue);
+            return Task.CompletedTask;
+
+        }
+    }
+
+    public class EnumerationConverter : JsonConverter<Enumeration>
+    {
+        public override void WriteJson(JsonWriter writer, Enumeration value, JsonSerializer serializer)
+        {
+            writer.WriteValue(value.Value);
+        }
+
+        //public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+        //{
+        //    return typeof(Enumeration).MakeGenericType(existingValue?.GetType()).GetMethod(nameof(Enumeration.Parse))
+        //        ?.Invoke(null, new[] { existingValue });
+        //}
+
+        public override Enumeration ReadJson(JsonReader reader, Type objectType, Enumeration existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            return typeof(Enumeration).GetMethod(nameof(Enumeration.Parse)).MakeGenericMethod(objectType)
+                .Invoke(null, new[] { (string)reader.Value }) as Enumeration;
+        }
+    }
+}
---
+++ Extensions/Extensions.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using Geexbox.Common;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Geexbox.Extensions
+{
+    public static class Extensions
+    {
+        public static string ComputeMd5(this string value)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            var bytValue = System.Text.Encoding.UTF8.GetBytes(value);
+            var bytHash = md5.ComputeHash(bytValue);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+
+        public static string ComputeMd5(this byte[] bytes)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            var bytHash = md5.ComputeHash(bytes);
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+
+        public static string ComputeMd5(this Stream stream)
+        {
+            if (!stream.CanSeek)
+            {
+                throw new NotSupportedException("流类型不支持该方法,请使用带有out参数的重载");
+            }
+            var position = stream.Position;
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            var bytHash = md5.ComputeHash(stream);
+            stream.Position = position;
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+
+        public static string ComputeMd5WithOutStream(this Stream stream, out MemoryStream outStream)
+        {
+            var position = stream.Position;
+            outStream = new MemoryStream();
+            stream.CopyTo(outStream);
+            outStream.Position = position;
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            var bytHash = md5.ComputeHash(outStream);
+            outStream.Position = position;
+            md5.Clear();
+            string sTemp = "";
+            for (int i = 0; i < bytHash.Length; i++)
+            {
+                sTemp += bytHash[i].ToString("X").PadLeft(2, '0');
+            }
+            return sTemp.ToLower();
+        }
+        /// <summary>
+        /// 将浏览器FileReader读取的dataurl转化成标准的base64字符串,同时获取其文件扩展名
+        /// </summary>
+        /// <param name="base64WithWebHeader"></param>
+        /// <param name="fileExt"></param>
+        /// <returns></returns>
+        public static (string base64, string ext) DataUrlToBase64WithExt(this string base64WithWebHeader)
+        {
+            var splitIndex = base64WithWebHeader.IndexOf(",");
+            var fileExt = "";
+            if (splitIndex <= 0)
+            {
+                throw new Exception("未找到Base64的Web Header");
+            }
+            var webHeader = base64WithWebHeader.Split(new[] { ',' }, 2)[0];
+            if (webHeader.Contains("jpeg"))
+            {
+                fileExt = "jpeg";
+                return (base64WithWebHeader.Substring(splitIndex + 1), fileExt);
+            }
+            if (webHeader.Contains("png"))
+            {
+                fileExt = "png";
+                return (base64WithWebHeader.Substring(splitIndex + 1), fileExt);
+            }
+            throw new Exception("不支持的Base64文件格式");
+        }
+        public static JsonSerializerSettings DefaultSerializeSettings { get; set; } = new JsonSerializerSettings()
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            Converters = new List<JsonConverter>()
+            {
+                new StringEnumConverter(),
+                new EnumerationConverter(),
+            },
+        };
+
+        public static JsonSerializerSettings IgnoreErrorSerializeSettings { get; set; } = new JsonSerializerSettings()
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            Error = (sender, args) =>
+            {
+                args.ErrorContext.Handled = true;
+            },
+            TypeNameHandling = TypeNameHandling.Auto,
+            TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
+            DateFormatString = "yyyy-MM-dd HH:mm:ss",
+            Converters = new List<JsonConverter>()
+            {
+                new StringEnumConverter()
+            },
+        };
+
+        public static string ToJson(this object @this, bool ignoreError = true)
+        {
+            if (ignoreError)
+            {
+                return JsonConvert.SerializeObject(@this, IgnoreErrorSerializeSettings);
+            }
+            else
+            {
+                return JsonConvert.SerializeObject(@this, DefaultSerializeSettings);
+            }
+        }
+
+        public static T ToObject<T>(this string @this, bool ignoreError = true)
+        {
+            if (ignoreError)
+            {
+                return JsonConvert.DeserializeObject<T>(@this, IgnoreErrorSerializeSettings);
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<T>(@this, DefaultSerializeSettings);
+            }
+        }
+    }
+}
